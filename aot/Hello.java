6631a040a588731c2023940888b6959f17f0832c
Steven Shaw <steven@steshaw.org>
2021-06-25 13:14:58 +1000

    AOT example

---
+++ aot/Hello.java
@@ -0,0 +1,10 @@
+public class Hello {
+
+    public static void main(String[] argv) {
+        System.out.println(message());
+    }
+
+    public static String message() {
+        return "Hello world!";
+    }
+}
